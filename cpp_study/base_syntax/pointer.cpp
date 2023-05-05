0d7d778e4122ef41535f8793046fb50ec0f49671
lds <2696612575@qq.com>
2023-05-05 12:10:44 +0800

    1. cpp指针的定义与使用

---
+++ cpp_study/base_syntax/pointer.cpp
@@ -0,0 +1,18 @@
+//
+// Created by lds on 23-5-5.
+//
+
+#include "../my_header.h"
+
+int main() {
+    int a = 1;
+    char b = 'c';
+    bool c = false;
+    string d = "time";
+
+    cout << "a=" << &a << endl;
+    cout << "b=" << &b << endl;
+    cout << "c=" << &c << endl;
+    cout << "d=" << &d << endl;
+    return 0;
+}
---
+++ cpp_study/my_header.h
@@ -0,0 +1,12 @@
+//
+// Created by lds on 23-5-5.
+//
+
+#ifndef CPP_STUDY_MY_HEADER_H
+#define CPP_STUDY_MY_HEADER_H
+
+#include <iostream>
+
+using namespace std;
+
+#endif //CPP_STUDY_MY_HEADER_H
