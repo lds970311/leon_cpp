f3b6b7891af49ff0f4af4b186f952c30d9f45e92
lds <2696612575@qq.com>
2023-05-27 18:59:45 +0800

    1. flask师徒，模板的使用

--- py_evan/10_flask/fisrt_flask/app.py
+++ py_evan/10_flask/fisrt_flask/app.py
@@ -3,13 +3,15 @@
 # author: evan
 import json
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, \
+    make_response, redirect
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates')
 
 
 @app.route('/')
 def hello_world():  # put application's code here
+    redirect('/hello')
     return 'Hello 145!'
 
 
@@ -21,6 +23,26 @@ def hello():
     }
 
     return render_template('hello.html', user=user)
+
+
+@app.route('/page/<int:id>')
+def page(id):
+    # print(json.dumps(current_app))
+    # print(json.dumps(g))
+    print(request.method)
+    print(request.headers)
+    print(request.args)
+    return f'id:{id}'
+
+
+@app.before_first_request
+def before_fr():
+    print('before_first_request')
+
+
+@app.before_request
+def before_request():
+    print('before request')
 
 
 @app.route('/man')
@@ -35,5 +57,19 @@ def man():
     return json.dumps(man)
 
 
+@app.route('/resp')
+def response():
+    return make_response()
+
+
+@app.route('/html')
+def render_html():
+    with open('./templates/test.html', 'r') as f:
+        content = f.read()
+
+    return make_response(content, 200)
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+    print(app.url_map)
