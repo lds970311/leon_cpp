dd8b5ab8fc7b75e35416f4705b0ebaec92c28fd2
lds <2696612575@qq.com>
2023-06-08 14:49:23 +0800

    1. 解决反扒：js逆向

--- py_evan/12_spider/spider_01/proxy_config.py
+++ py_evan/12_spider/spider_01/proxy_config.py
@@ -2,6 +2,8 @@
 # time: 2023/5/30
 # author: evan
 # 隧道域名:端口号
+import random
+
 tunnel = "a741.kdltps.com:15818"
 
 # 用户名密码方式
@@ -12,6 +14,13 @@ proxies = {
     "https": "http://%(user)s:%(pwd)s@%(proxy)s/" % {"user": username, "pwd": password, "proxy": tunnel}
 }
 
+uas = [
+    "User-Agent:Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+    "User-Agent:Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0",
+    "User-Agent:Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+    "User-Agent:Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+]
+
 headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+    'User-Agent': random.choice(uas)
 }
