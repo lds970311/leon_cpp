c9f52b05a7eb17365d06d3c01d59fcc4a461b1d3
lds <2696612575@qq.com>
2023-07-11 20:28:30 +0800

    1. django 表单的验证 2。 使用orm实现表单 3. 文件上传

--- py_evan/11_django/first_django/accounts/urls.py
+++ py_evan/11_django/first_django/accounts/urls.py
@@ -2,13 +2,14 @@
 # time: 2023/6/23
 # author: evan
 
-from django.urls import path
-
 from accounts import views
+from django.urls import path
 
 urlpatterns = [
     path('user/info/', views.user_info, name='user_info'),
     path('user/register/', views.user_register, name='user_register'),
     path('user/signup/', views.user_register_transaction, name='user_register_transaction'),
-    path('user/login/', views.login, name='login')
+    path('user/login/', views.login, name='login'),
+    path('user/change/', views.user_change, name='change'),
+    path('user/upload/', views.upload, name='upload')
 ]
